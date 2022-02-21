098c78afc3752ac2ecbb98c3bf9f38970d819326
emh33 <55838653+emh33@users.noreply.github.com>
2022-02-21 18:00:22 +0000

    dummy login

--- app/src/main/java/com/example/matsedillvikunnar/MainActivity.java
+++ app/src/main/java/com/example/matsedillvikunnar/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.matsedillvikunnar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.matsedillvikunnar.activities.MyPage;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView email =(TextView) findViewById(R.id.login_email);
+        TextView password =(TextView) findViewById(R.id.login_password);
+        Button loginbtn = (Button) findViewById(R.id.login_btn);
+
+        //Netfang: admin
+        //Lykilorð: admin
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(email.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    startActivity(new Intent(MainActivity.this, MyPage.class));
+                    Toast.makeText(MainActivity.this,"Tókst að innskrá",Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(MainActivity.this,"Netfang eða lykilorð vitlaust",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/matsedillvikunnar/activities/MyPage.java
@@ -0,0 +1,15 @@
+package com.example.matsedillvikunnar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.matsedillvikunnar.R;
+
+public class MyPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page);
+    }
+}
