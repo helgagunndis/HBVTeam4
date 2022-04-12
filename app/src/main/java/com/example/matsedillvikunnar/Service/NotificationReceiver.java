9622a680c51f7e6cefa05df331e39b05d19a0d31
Helga Gunndís <helgagunndis@gmail.com>
2022-04-12 22:19:48 +0000

    notification
    reminder to make meal plan

---
+++ app/src/main/java/com/example/matsedillvikunnar/Service/NotificationReceiver.java
@@ -0,0 +1,34 @@
+package com.example.matsedillvikunnar.Service;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.matsedillvikunnar.R;
+import com.example.matsedillvikunnar.ui.Activities.MealPlanActivity;
+
+public class NotificationReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent resultIntent = new Intent(context, MealPlanActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification")
+
+                .setSmallIcon(R.drawable.ic_baseline_announcement_24)
+                .setContentTitle("Útbúa matseðil!")
+                .setContentText("Ertu búin að útbúa matseðil fyrir vikuna?")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(200, builder.build());
+
+
+    }
+}
