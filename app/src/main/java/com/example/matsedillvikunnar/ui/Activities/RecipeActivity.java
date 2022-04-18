9373a948bab7b1cf8c207da89ac6b1dd6d8c3a66
EsterJenny <ejb7@hi.is>
2022-04-18 14:50:34 +0000

    ingredients birtist

--- app/src/main/java/com/example/matsedillvikunnar/ui/Activities/RecipeActivity.java
+++ app/src/main/java/com/example/matsedillvikunnar/ui/Activities/RecipeActivity.java
@@ -6,15 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.matsedillvikunnar.EntityClass.Ingredient;
 import com.example.matsedillvikunnar.EntityClass.Recipe;
 import com.example.matsedillvikunnar.R;
 import com.example.matsedillvikunnar.Service.RecipeService;
@@ -22,17 +26,21 @@ import com.example.matsedillvikunnar.networking.NetworkCallback;
 import com.example.matsedillvikunnar.networking.NetworkManager;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeActivity extends AppCompatActivity {
     private TextView mRecipeName;
     private ImageView mRecipeImage;
+    private TextView mRecipeMethod;
+    private ListView mRecipeIngredients;
+    private TextView mRecipeTime;
+    private TextView mRecipeServings;
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
     List<Recipe> recipeList;
     private int mRecipe;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+    ArrayList ingredientList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +62,6 @@ public class RecipeActivity extends AppCompatActivity {
         }
 
         mRecipe = Integer.parseInt(recipeID);
-        Log.d(TAG, "recipe id"+ mRecipe);
-
         getRecipe();
     }
 
@@ -78,10 +84,25 @@ public class RecipeActivity extends AppCompatActivity {
 
                 mRecipeName = (TextView) findViewById(R.id.recipeName);
                 mRecipeImage = (ImageView) findViewById(R.id.recipe_image);
+                mRecipeMethod = (TextView) findViewById(R.id.recipe_method);
+                mRecipeIngredients = (ListView) findViewById(R.id.recipe_ingredients);
+                mRecipeTime = (TextView)findViewById(R.id.recipe_time);
+                mRecipeServings = (TextView) findViewById(R.id.recipe_servings);
 
                 mRecipeName.setText(recipe.getRecipeTitle());
-                mRecipeName.setText(recipe.getRecipeTitle());
-                Picasso.get().load(recipe.getRecipeImage()).into(mRecipeImage);
+                Picasso.get().load(recipe.getRecipeImage()).fit().into(mRecipeImage);
+                mRecipeMethod.setText(recipe.getRecipeMethod());
+                mRecipeServings.setText("Fyrir " + recipe.getRecipeServings());
+                mRecipeTime.setText(recipe.getRecipeTime());
+
+                ingredientList = new ArrayList();
+                for (Ingredient ingredient:recipe.getIngredients()) {
+                    ingredientList.add(ingredient.getAmount()+ " " +ingredient.getIngredientInfo().getIngredientName());
+                }
+
+                mRecipeIngredients = (ListView)findViewById(R.id.recipe_ingredients);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(RecipeActivity.this, R.layout.listview_ingredients,R.id.ingredient, ingredientList);
+                mRecipeIngredients.setAdapter(arrayAdapter);
 
             }
             @Override
